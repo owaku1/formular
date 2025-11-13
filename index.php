c1ed32829f29f4e9f97e380b0161fd44b3d8606f
Cong Tien Dung Nguyen <Dung.Nguyen@student.souepl.cz>
2025-11-13 13:41:16 +0100

    asca

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrace</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js" defer></script>
+</head>
+<body>
+    <main>
+        <section>
+            <div class="form">
+            <div class="form-header">
+                <img src="/images/apex.png" alt="LogoApex">
+                <h2>Create an account</h2>
+            </div>
+            </div>
+            <form action="a">
+                <div class="form-main">
+                    <div class="form-main-inputs">
+                        <div class="form-main-inputs-label">
+                           
+                        </div>
+                    </div>
+                </div>
+        </section>
+        
+
+    </main>
+    
+</body>
+</html>
