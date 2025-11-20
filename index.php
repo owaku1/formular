3c22c28b1df5b84a0df1a8345cc7edd29c227264
owaku1 <nguyencongtiendung37@gmail.com>
2025-11-20 07:11:50 +0100

    ad

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrace</title>
+
     <link rel="stylesheet" href="style/style.css">
     <script src="script/script.js" defer></script>
 </head>
@@ -11,23 +12,49 @@
     <main>
         <section>
             <div class="form">
-            <div class="form-header">
-                <img src="/images/apex.png" alt="LogoApex">
-                <h2>Create an account</h2>
-            </div>
-            </div>
-            <form action="a">
-                <div class="form-main">
-                    <div class="form-main-inputs">
-                        <div class="form-main-inputs-label">
-                           
+                <div class="form-header">
+                    <img src="/images/apexlogo.png" alt="LogoApex">
+                    <h2>Create an account</h2>
+                </div>
+                <form>
+                    <div class="form-main">
+                        <div class="form-main-inputs">
+                            <div class="form-main-inputs-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                </svg>
+                                <label for="name">Username</label>
+                            </div>
+                            <input type="text" placeholder="Username (alphanumeric only)">
+                        </div>
+                        <div class="form-main-inputs">
+                            <div class="form-main-inputs-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                </svg>
+                                <label for="name">Password</label>
+                            </div>
+                            <input type="text" placeholder="Enter your password (min 6 characters)">
+                        </div>
+                        <div class="form-main-inputs">
+                            <div class="form-main-inputs-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                </svg>
+                                <label for="name">Confirm Password</label>
+                            </div>
+                            <input type="text" placeholder="Confirm your password">
                         </div>
                     </div>
-                </div>
-        </section>
-        
+                    <div class="form-control">
 
+                    </div>
+                    <div class="form-buttons">
+
+                    </div>       
+                </form>
+            </div>
+        </section>
     </main>
-    
 </body>
 </html>
